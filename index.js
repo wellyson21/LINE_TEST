e1bc5118db6b1dadff424a0660d1fadb5a4e121d
wellyson21 <wellysonwcr21@gmail.com>
2017-03-27 19:17:06 -0300

    s
    s

--- index.js
+++ index.js
@@ -15,12 +15,8 @@ app.get('/', function(request, response) {
 
 
 
-
-
-
-
 var lineBot = require('linebot'),
-    channelId = 1503342448,
+    channelId = '1503342448',
     channelSecret = '15f3c6563c1b68b221aca3a7f0bac0ad',
     token = 'pvCPP5tm40b9XjlyI3yvG3U4/G4sKZ4Y+ic2z4iFXRJAIZXErV7xHSGjoXR5E/S1JMRUjZonFvNTSRVQjHhFk7KEkrsrROJaoYE91hQw/Y6McRhuJP4Hl6boDnT0j3XJR372Z9LJXKgIWtZzszDJlgdB04t89/1O/w1cDnyilFU=';
 
@@ -33,12 +29,12 @@ var bot = lineBot({
 const linebotParser = bot.parser();
 app.post('/', linebotParser);
 
-bot.on('message', function(event) {
+bot.on('message', function(event){
   console.log(event);
 });
 
 
-var server = app.listen(process.env.PORT || 8080, function() {
+var server = app.listen(3000, function(){
   var port = server.address().port;
   console.log("App now running on port", port);
 });
