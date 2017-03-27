f9fc519630facc88c6d721068e874520ab158a95
wellyson21 <wellysonwcr21@gmail.com>
2017-03-27 16:27:56 -0300

    line_test
    firstChange

--- index.js
+++ index.js
@@ -6,14 +6,11 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-//app.set('views', __dirname + '/views');
-//app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  //response.render('pages/index');
-  
-  console.log(10);
-  
+  response.render('pages/index');
 });
 
 app.listen(app.get('port'), function() {
