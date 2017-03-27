15aa99cff6a65ca7cfb36362eec56a7b12f43561
wellyson21 <wellysonwcr21@gmail.com>
2017-03-27 17:54:45 -0300

    s
    s

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 var express = require('express');
 var app = express();
 
-// app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5000));
 
-// app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
 
 var linebot = require('linebot'),
-    channelId = 1503342448,
-    channelSecret = '15f3c6563c1b68b221aca3a7f0bac0ad',
-    token = 'pvCPP5tm40b9XjlyI3yvG3U4/G4sKZ4Y+ic2z4iFXRJAIZXErV7xHSGjoXR5E/S1JMRUjZonFvNTSRVQjHhFk7KEkrsrROJaoYE91hQw/Y6McRhuJP4Hl6boDnT0j3XJR372Z9LJXKgIWtZzszDJlgdB04t89/1O/w1cDnyilFU=';
+  channelId = 1503342448,
+  channelSecret = '15f3c6563c1b68b221aca3a7f0bac0ad',
+  token = 'pvCPP5tm40b9XjlyI3yvG3U4/G4sKZ4Y+ic2z4iFXRJAIZXErV7xHSGjoXR5E/S1JMRUjZonFvNTSRVQjHhFk7KEkrsrROJaoYE91hQw/Y6McRhuJP4Hl6boDnT0j3XJR372Z9LJXKgIWtZzszDJlgdB04t89/1O/w1cDnyilFU=';
 
 var bot = linebot({
   channelId: channelId,
@@ -19,33 +22,17 @@ var bot = linebot({
 });
 
 
-bot.on('message', function(event) {
-  console.log(event);
-});
-
-const linebotParser = bot.parser();
-app.post('/', linebotParser);
 
 
 
-// var server = app.listen(process.env.PORT || app.get('port'), function() {
-//   var port = server.address().port;
-//   console.log("App now running on port", port);
-// });
-
-
-// // views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-//
 
 app.get('/', function(request, response) {
-  // response.render('pages/index');
-  console.log('home');
+  response.render('pages/index');
 });
+
+
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
