d83a789a482dfa3c28380cc812574766a043b474
wellyson21 <wellysonwcr21@gmail.com>
2017-03-28 04:35:22 -0300

    s
    s

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ var server = app.listen(process.env.PORT, function() {
 
       if(err)return '';
 
-      if(!result){
+      if(result.length < 1){
         client.query('create table allData(id serial primary key,anyText varchar(200))');
         done(err);
       }
